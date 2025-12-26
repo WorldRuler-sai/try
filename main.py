bef92ad0c755778c44d86f441c2c2ecba1bff6ca
WorldRuler-sai <kltulasi10@gmail.com>
2025-12-26 08:20:40 +0400

    Modify index route and comment out home route
    Updated index route to render 'web1.html'. Commented out home route.

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from tkinter import *
 
 app = Flask(__name__)
 
-@app.route('/<name>')
-def home(name):
-     return render_template('web1.html')
+# @app.route('/<name>')
+# def home(name):
+     # return render_template('web1.html')
 
 @app.route('/')
 def index():
-    return "Hi there! Welcome to the Flask app."
+    return render_template('web1.html')
 
 if __name__ == '__main__':
     app.run()
+
